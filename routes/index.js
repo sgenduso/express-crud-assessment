9a4d68bbca2227fa8fc5c07d076eb181e4afe422
Sam Genduso <sam.genduso@gmail.com>
2015-07-23 10:30:27 -0600

    local mongodb set up

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var db = require('monk')(process.env.MONGOLAB_URI || process.env.ZINE_DB);
+var db = require('monk')(/*process.env.MONGOLAB_URI || */process.env.ZINE_DB);
+var articles = db.get('articles');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Zine' });
 });
 
 router.get('/articles/new', function (req, res, next) {
   res.render('new', {title: 'The Zine'});
+});
+
+router.post('/articles/new', function (req, res, next) {
+  articles.insert(req.body);
+    res.redirect('/');
 });
 
 module.exports = router;
