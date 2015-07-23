d79417611f200c45e0b5a66bd2dfdc251245e82c
Sam Genduso <sam.genduso@gmail.com>
2015-07-23 10:09:16 -0600

    new article form mostly styled

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')(process.env.MONGOLAB_URI || process.env.ZINE_DB);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The Zine' });
+});
+
+router.get('/articles/new', function (req, res, next) {
+  res.render('new', {title: 'The Zine'});
 });
 
 module.exports = router;
